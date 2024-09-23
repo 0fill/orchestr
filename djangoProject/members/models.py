1a50022619dda126b4f43509122226591e3b07fb
0fill <fikrosek@seznam.cz>
2024-09-23 08:22:38 +0200

    s

--- djangoProject/members/models.py
+++ djangoProject/members/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=50)
+    email = models.EmailField()
